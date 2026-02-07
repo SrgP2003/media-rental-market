cfb2a347010475b8300e2567a21656c6c3ad499f
sergio.arevalo <sergioarepalacios9264@gmail.com>
2026-02-06 23:13:59 -0600

    Creacion del modelo Media

---
+++ app/Models/Media.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Media extends Model
+{
+    use HasFactory;
+
+    protected $table = 'media'; //Tabla 'media'
+
+    protected $fillable = [ //Campos que se llenaran masivamente
+        'name',
+        'type',
+        'location',
+        'dimensions',
+        'price_per_day',
+        'status'
+    ];
+    //Para tipar datos automaticamente
+    protected $casts = [
+        'price_per_day' => 'decimal:2',
+        'status' => 'boolean',
+    ];
+    //Relaciones con otras entidades: un medio publicitario tendra muchas RESERVAS (Bookings)
+    public function Bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+}
